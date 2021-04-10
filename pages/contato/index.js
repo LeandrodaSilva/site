3e900779e69fcc17859792371baa28668902e5b9
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2021-04-10 15:04:16 -0300

    Added contact page

---
+++ pages/contato/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function Index() {
+  return (
+    <Layout>
+      <Head>
+        <title>Informações para contato</title>
+      </Head>
+    </Layout>
+  )
+}
