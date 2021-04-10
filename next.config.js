9b52f7a9c713f64f5e5d6281dda2e2f8e2ac139e
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2021-04-10 20:34:20 -0300

    Added multi language support

--- next.config.js
+++ next.config.js
@@ -3,5 +3,26 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
     pwa: {
         dest: 'public'
-    }
+    },
+    i18n: {
+        // These are all the locales you want to support in
+        // your application
+        locales: ['en-US', 'pt-BR'],
+        // This is the default locale you want to be used when visiting
+        // a non-locale prefixed path e.g. `/hello`
+        defaultLocale: 'en-US',
+        // This is a list of locale domains and the default locale they
+        // should handle (these are only required when setting up domain routing)
+        // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+        domains: [
+          {
+            domain: 'leandrodasilva.dev',
+            defaultLocale: 'en-US',
+          },
+          {
+            domain: 'leandrodasilva.pt',
+            defaultLocale: 'pt-BR',
+          },
+        ],
+      },
 })
