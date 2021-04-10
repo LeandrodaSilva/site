fbfaf9c33efeec64fd8c1126454ea0b282286532
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2021-04-10 19:45:54 -0300

    Added pwa

--- components/layout.tsx
+++ components/layout.tsx
@@ -17,6 +17,8 @@ export default function Layout({
   return (
     <div className={styles.container}>
       <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000"/>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
