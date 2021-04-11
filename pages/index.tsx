cc8924e09276fdf1528e96d32436e3a477b2ce6c
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2021-04-11 16:23:21 -0300

    Added links to social medias

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
@@ -5,7 +6,9 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
-
+import * as icons from "@fortawesome/free-solid-svg-icons"
+import * as brandsIcons from "@fortawesome/free-brands-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
@@ -30,29 +33,41 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Developer</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Links</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
+          <li className={utilStyles.listItem}>
+            <FontAwesomeIcon height={15} icon={icons.faEnvelope} />{'  '}
+            <a href="mailto:ld_silva13@hotmail.com?subject=Olá%20Leandro">ld_silva13@hotmail.com</a>
+          </li>
+
+          <li className={utilStyles.listItem}>
+            <FontAwesomeIcon height={15} icon={brandsIcons.faGithub} />{'  '}
+            <a href="https://github.com/LeandrodaSilva">LeandroDaSilva</a>
+          </li>
         </ul>
       </section>
+
+      {
+        (allPostsData.length > 0) &&
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      }
     </Layout>
   )
 }
