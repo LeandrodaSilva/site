fb91b8875fad7a28c6bfa294479cb47a00ac7d7f
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2021-04-11 19:15:22 -0300

    Added some changes to improve score

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 const withPWA = require('next-pwa')
- 
+
 module.exports = withPWA({
     pwa: {
         dest: 'public'
+    },
+    i18n: {
+        locales: ['en-US'],
+        defaultLocale: 'en-US',
+        localeDetection: false,
+        domains: [
+            {
+                domain: 'leandrodasilva.dev',
+                defaultLocale: 'en-US',
+            }
+        ],
     },
 })
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -60,8 +60,10 @@ function App({ Component, pageProps, darkMode }: AppProps) {
         <title/>
       </Head>
       <GlobalStyles />
-      <div className="switch-container">
+      <div className="switch-container" aria-hidden="true">
         <Switch
+          aria-hidden="true"
+          id="switch"
           className="switch"
           onColor="#3b3b3b"
           offColor="#d1d1d1"
