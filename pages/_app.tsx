c355f142c72aa26c04a5a08c6e1c9d3f520d4384
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2021-04-11 19:25:38 -0300

    Added switch label

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -61,7 +61,7 @@ function App({ Component, pageProps, darkMode }: AppProps) {
       </Head>
       <GlobalStyles />
       <div className="switch-container">
-        <label className="hidden" htmlFor="switch">
+        <label className="hidden" for="switch">
           Change theme color
         </label>
         <Switch
