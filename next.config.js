5eb0068f640d17a46bb3943bffcc1e66fe1935ac
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2021-04-11 11:54:36 -0300

    Some changes in location and neon styles

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ module.exports = withPWA({
     i18n: {
         // These are all the locales you want to support in
         // your application
-        locales: ['en-US', 'pt-BR'],
+        locales: ['en-US'],
         // This is the default locale you want to be used when visiting
         // a non-locale prefixed path e.g. `/hello`
         defaultLocale: 'en-US',
-        localeDetection: true,
+        localeDetection: false,
         // This is a list of locale domains and the default locale they
         // should handle (these are only required when setting up domain routing)
         // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
@@ -19,10 +19,6 @@ module.exports = withPWA({
           {
             domain: 'leandrodasilva.dev',
             defaultLocale: 'en-US',
-          },
-          {
-            domain: 'leandrodasilva.dev',
-            defaultLocale: 'pt-BR',
           }
         ],
       },
