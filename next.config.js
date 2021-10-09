01ef942e3397aaf219f3e1252aa4c89273c7da67
LeandroDaSilva <Av@rogue11076>
2021-10-09 13:44:03 -0300

    Deleted files

--- next.config.js
+++ next.config.js
@@ -4,15 +4,4 @@ module.exports = withPWA({
     pwa: {
         dest: 'public'
     },
-    // i18n: {
-    //     locales: ['en-US'],
-    //     defaultLocale: 'en-US',
-    //     localeDetection: false,
-    //     domains: [
-    //         {
-    //             domain: 'leandrodasilva.dev',
-    //             defaultLocale: 'en-US',
-    //         }
-    //     ],
-    // },
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,9 @@ import React from "react";
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-// import { getSortedPostsData } from '../lib/posts'
-// import Link from 'next/link'
-// import Date from '../components/date'
-// import { GetStaticProps } from 'next'
 import * as icons from "@fortawesome/free-solid-svg-icons";
 import * as brandsIcons from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   const allPostsData = getSortedPostsData()
-//   return {
-//     props: {
-//       allPostsData,
-//     }
-//   }
-// }
-
-// {
-//   allPostsData,
-// }: {
-//   allPostsData: {
-//     date: string
-//     title: string
-//     id: string
-//   }[],
-// }
 
 export default function Home() {
   return (
@@ -52,26 +29,6 @@ export default function Home() {
           </li>
         </ul>
       </section>
-
-      {/*{*/}
-      {/*  (allPostsData.length > 0) &&*/}
-      {/*  <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>*/}
-      {/*    <h2 className={utilStyles.headingLg}>Blog</h2>*/}
-      {/*    <ul className={utilStyles.list}>*/}
-      {/*      {allPostsData.map(({ id, date, title }) => (*/}
-      {/*        <li className={utilStyles.listItem} key={id}>*/}
-      {/*          <Link href={`/posts/${id}`}>*/}
-      {/*            <a>{title}</a>*/}
-      {/*          </Link>*/}
-      {/*          <br />*/}
-      {/*          <small className={utilStyles.lightText}>*/}
-      {/*            <Date dateString={date} />*/}
-      {/*          </small>*/}
-      {/*        </li>*/}
-      {/*      ))}*/}
-      {/*    </ul>*/}
-      {/*  </section>*/}
-      {/*}*/}
     </Layout>
   );
 }
