0e76cab34a8b72de02197e82d40157a002b3e69c
LEANDRO DA SILVA <leandroda.silva@sebsa.com.br>
2024-08-27 12:57:00 -0300

    feat: Menina topo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // serve static files from the public directory
+  // https://nextjs.org/docs/api-reference/next.config.js/static-files
+  async rewrites() {
+    return [
+      {
+        source: "/latest/:path*",
+        destination: "/public/latest/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
